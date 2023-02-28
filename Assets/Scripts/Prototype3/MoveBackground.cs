0c8dbb7b569d6b7f2e973caab312b6a81b390960
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2023-02-28 19:27:03 -0300

    feat: adds looping background

---
+++ Assets/Scripts/Prototype3/MoveBackground.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public float speed;
+    
+    private float originalX;
+    private Vector2 backgroundSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundSize = GetComponent<SpriteRenderer>().size;
+        originalX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= originalX - backgroundSize.x/2)
+            this.transform.position = new Vector3(originalX, 9.5f, 4f);
+    }
+}
